30d9814faa87da8b277eb57c99e5dd48e14ba9d4
Owen Kahn <owen@okahn.net>
2022-12-07 14:03:35 -0500

    Committing current working state
    Next step is working code for "taxonomies"

--- src/main.rs
+++ src/main.rs
@@ -14,17 +14,21 @@ pub mod dfa;
 /// Representations of transducers and DFAs.
 pub mod transducer;
 
+use combi::{Cycle, UCycle, VCycle};
+use graphviz_rust::dot_generator::*;
+use graphviz_rust::dot_structures::*;
 use graphviz_rust::{
     cmd::{CommandArg, Format},
     exec,
     printer::PrinterContext,
 };
-use rustc_hash::FxHashMap;
+use rustc_hash::FxHashSet;
 use std::env;
-use transducer::classify_transducers;
+use transducer::{classify_transducers, AllTransducers, Transducer};
 
-fn write_orbit_trees(size: usize, depth: usize) {
-    let res = classify_transducers(size, depth);
+/// TODO
+pub fn write_orbit_trees(depth: usize) {
+    let res = classify_transducers(depth);
     let mut ctx = PrinterContext::default();
     ctx.always_inline();
     for (i, m) in res.iter().enumerate() {
@@ -42,12 +46,162 @@ fn write_orbit_trees(size: usize, depth: usize) {
     }
 }
 
+fn min_necklace(x: Vec<usize>) -> Vec<usize> {
+    let mut min = x.clone();
+    for offset in 0..x.len() {
+        let xs: Vec<_> = (0..x.len()).map(|i| x[(i + offset) % x.len()]).collect();
+        if xs < min {
+            min = xs
+        }
+    }
+    return min;
+}
+
+fn necklaces(width: usize) -> Vec<Vec<usize>> {
+    let mut res = FxHashSet::default();
+    for x in VCycle::<usize, UCycle<usize>>::new(vec![2; width], true) {
+        res.insert(min_necklace(x));
+    }
+    let mut out: Vec<_> = res.into_iter().collect();
+    out.sort();
+    return out;
+}
+
+fn is_odd(x: &[usize]) -> bool {
+    x.iter().filter(|&&k| k == 0).count() % 2 == 1
+}
+
 fn main() {
+    let mut seen = FxHashSet::default();
+    for (i, m) in AllTransducers::new().enumerate() {
+        let m2 = m.minimize().canonicalize();
+        if seen.contains(&m2) {
+            continue;
+        } else {
+            seen.insert(m2);
+        }
+        if m.len() > 2 {
+            break;
+        }
+        println!("{i} {m:?}")
+    }
     let args: Vec<String> = env::args().collect();
-    let size = args[1].parse::<usize>().unwrap();
-    let depth = args[2].parse::<usize>().unwrap();
+    let depth = args[1].parse::<usize>().unwrap();
+    let m = Transducer::new(vec![vec![0, 1], vec![1, 0]], vec![1, 0]);
+    let m2 = Transducer::new(vec![vec![0, 1], vec![1, 0]], vec![0, 1]);
+    println!(
+        "{:?}",
+        [
+            &m,
+            &m.product(&m).minimize(),
+            &m.product(&m).product(&m).minimize()
+        ]
+    );
+    m.detailed_orbit_tree(8);
+    m.product(&m).product(&m).detailed_orbit_tree(8);
+    let mut g = graph!(strict di id!());
+    let width = depth;
+    let mut g_even = subgraph!(id!("cluster_s0"));
+    let mut g_odd = subgraph!(id!("cluster_s1"));
+    g_even.stmts.push(stmt!(attr!("color", "white")));
+    g_odd.stmts.push(stmt!(attr!("color", "white")));
+    for x in necklaces(width) {
+        let l_label: String = x.iter().map(|x| x.to_string()).collect();
+        if is_odd(&x) {
+            if x == vec![1; width] {
+                g_odd.stmts.push(stmt!(
+                    node!(esc l_label; /*(attr!("shape", "circle"),*/ attr!("root", "true"))
+                ));
+            } else {
+                g_odd
+                    .stmts
+                    .push(stmt!(node!(esc l_label /*;attr!("shape", "circle"),*/)));
+            }
+        } else {
+            if x == vec![1; width] {
+                g_even.stmts.push(stmt!(
+                    node!(esc l_label; /*(attr!("shape", "circle"),*/ attr!("root", "true"))
+                ));
+            } else {
+                g_even
+                    .stmts
+                    .push(stmt!(node!(esc l_label/*;attr!("shape", "circle"),*/)));
+            }
+        }
+    }
+    g.add_stmt(stmt!(g_even));
+    g.add_stmt(stmt!(g_odd));
+    for y in necklaces(width) {
+        for offset in 0..y.len() {
+            let x: Vec<_> = (0..y.len()).map(|i| y[(i + offset) % y.len()]).collect();
+            let color: Attribute;
+            let constraint: Attribute;
+            let l_label: String = y.iter().map(|x| x.to_string()).collect();
+            let mut t0 = vec![0];
+            let mut t1 = vec![1];
+            let mut o0 = Vec::new();
+            let mut o1 = Vec::new();
+            for i in 0..width {
+                match x[i] {
+                    0 => {
+                        o0.push(m.step(&mut t0));
+                        o1.push(m.step(&mut t1));
+                    }
+                    1 => {
+                        // This relabeling is NEEDED because M2 is labeled differently than M1.
+                        o0.push(1 - m2.step(&mut t0));
+                        o1.push(1 - m2.step(&mut t1));
+                    }
+                    _ => panic!(),
+                }
+            }
+            if is_odd(&x) == is_odd(&o0) {
+                color = attr!("style", "solid");
+                // continue;
+            } else if is_odd(&x) {
+                color = attr!("style", "solid");
+            } else {
+                color = attr!("style", "solid");
+            }
+            constraint = attr!("constraint", ((!is_odd(&x)) || is_odd(&x) == is_odd(&o0)));
+            // constraint = attr!("constraint", "true");
+            let r0: String = min_necklace(o0).iter().map(|x| x.to_string()).collect();
+            let r1: String = min_necklace(o1).iter().map(|x| x.to_string()).collect();
+
+            if r1 == r0 {
+                g.add_stmt(stmt!(
+                    edge!(node_id!(esc l_label) => node_id!(esc r0); constraint, color)
+                ));
+            } else {
+                g.add_stmt(stmt!(
+                edge!(node_id!(esc l_label) => node_id!(esc r0); attr!("color", "blue"), constraint.clone(), color.clone())
+            ));
+                g.add_stmt(stmt!(
+                edge!(node_id!(esc l_label) => node_id!(esc r1); attr!("color", "red"), constraint, color)
+            ));
+            }
+            println!("{} {} {}", l_label, r0, r1);
+        }
+    }
+    let mut ctx = PrinterContext::default();
+    ctx.always_inline();
+    // let g = m.orbit_tree(depth);
+    exec(
+        g,
+        &mut ctx,
+        vec![
+            CommandArg::Custom("-Gdpi=300".to_string()),
+            CommandArg::Custom("-Kdot".to_string()),
+            CommandArg::Custom("-Goverlap=prism".to_string()),
+            CommandArg::Custom("-Gsplines=ortho".to_string()),
+            CommandArg::Format(Format::Png),
+            CommandArg::Output(format!("images/m_interest.png").to_string()),
+        ],
+    )
+    .unwrap();
     // write_orbit_trees(size, depth);
-    let res = classify_transducers(size, depth);
+    /*
+    let res = classify_transducers(depth);
     let mut cs: FxHashMap<usize, u64> = FxHashMap::default();
     for i in res.iter().map(|x| x.len()) {
         if cs.contains_key(&i) {
@@ -59,4 +213,5 @@ fn main() {
     let mut cs2: Vec<_> = cs.into_iter().collect();
     cs2.sort();
     println!("{} {:?}", res.len(), cs2);
+    */
 }
--- src/transducer.rs
+++ src/transducer.rs
@@ -33,13 +33,25 @@ fn repr(word: &[u8]) -> String {
 }
 
 impl Transducer {
-    fn step(&self, x: &mut Vec<u8>) {
+    /// Create a new transducer.
+    pub fn new(transition: Vec<Vec<usize>>, flip: Vec<u8>) -> Self {
+        Transducer { transition, flip }
+    }
+
+    /// TODO
+    pub fn len(&self) -> usize {
+        return self.transition.len();
+    }
+
+    /// TODO
+    pub fn step(&self, x: &mut Vec<u8>) -> usize {
         let mut state: usize = 0;
         for i in 0..x.len() {
             let c = x[i];
             x[i] = c ^ self.flip[state];
             state = self.transition[state][c as usize];
         }
+        return state;
     }
 
     fn min_word(&self, word: &[u8]) -> Vec<u8> {
@@ -220,7 +232,7 @@ impl Transducer {
                 new_flip.push((self.flip[i] + other.flip[j]) % 2);
                 for k in 0..2 {
                     nt.push(
-                        self.transition[i][k] * self.transition.len()
+                        self.transition[i][k] * other.transition.len()
                             + other.transition[j][(k + (self.flip[i] as usize)) % 2],
                     );
                 }
@@ -258,11 +270,76 @@ impl Transducer {
         return res;
     }
 
+    /// TODO
+    pub fn detailed_orbit_tree(&self, depth: usize) {
+        let mut cycle_trace = FxHashMap::default();
+        let mut prev: Vec<Vec<u8>> = vec![Vec::new()];
+        for _ in 1..depth + 1 {
+            let mut new_prev = Vec::new();
+            for word in &prev {
+                let mut target0 = word.clone();
+                let mut target1 = word.clone();
+                target0.push(0);
+                target1.push(1);
+                let mut seen1 = false;
+                let mut v = target0.clone();
+                let mut res = Vec::new();
+                new_prev.push(target0.clone());
+                loop {
+                    res.push(self.step(&mut v));
+                    if v == target1 {
+                        seen1 = true;
+                    }
+                    if v == target0 {
+                        break;
+                    }
+                }
+                cycle_trace.insert(target0, res);
+                if !seen1 {
+                    new_prev.push(target1.clone());
+                    res = Vec::new();
+                    let mut v = target1.clone();
+                    loop {
+                        res.push(self.step(&mut v));
+                        if v == target1 {
+                            break;
+                        }
+                    }
+                    cycle_trace.insert(target1, res);
+                }
+            }
+            prev = new_prev;
+        }
+        let mut out = cycle_trace.into_iter().collect::<Vec<_>>();
+        out.sort_by(|(x, _), (y, _)| match x.len().cmp(&y.len()) {
+            std::cmp::Ordering::Equal => x.cmp(&y),
+            other => other,
+        });
+        for elem in out {
+            let r0 = elem
+                .0
+                .iter()
+                .map(|&x| (('0' as u8) + x) as char)
+                .collect::<String>();
+            let r1 = elem
+                .1
+                .iter()
+                .map(|&x| (('A' as u8) + (x as u8)) as char)
+                /* .map(|x| match x {
+                    'A' => '$',
+                    'B' => '.',
+                    _ => '!',
+                }) */
+                .collect::<String>();
+            println!("{:depth$} {}", r0, r1);
+        }
+    }
+
     /// Create a graph corresponding to the transducer's orbit tree up to a
     /// given `depth`.
     pub fn orbit_tree(&self, depth: usize) -> Graph {
         let mut res = graph!(strict di id!();
-          node!(esc ""; attr!("label", "\u{03b5}"), attr!("shape", "circle")));
+          node!(esc ""; attr!("label", "\u{03b5}"), attr!("shape", "circle"), attr!("root", "true")));
         let mut words = vec![Vec::new()];
         for _ in 0..depth {
             let mut new_words = vec![];
@@ -288,7 +365,8 @@ impl Transducer {
         return res;
     }
 
-    fn residues(&self) -> Vec<Self> {
+    /// TODO
+    pub fn residues(&self) -> Vec<Self> {
         let mut remap_l = FxHashMap::default();
         let mut remap_r = FxHashMap::default();
         for i in 0..self.transition.len() {
@@ -311,19 +389,22 @@ impl Transducer {
 
 /// An iterator through all transducers of a particular size.
 pub struct AllTransducers {
-    state: TCycle<
-        Vec<u8>,
-        Vec<Vec<usize>>,
-        VCycle<u8, UCycle<u8>>,
-        VCycle<Vec<usize>, VCycle<usize, UCycle<usize>>>,
-    >,
+    state: (
+        usize,
+        TCycle<
+            Vec<u8>,
+            Vec<Vec<usize>>,
+            VCycle<u8, UCycle<u8>>,
+            VCycle<Vec<usize>, VCycle<usize, UCycle<usize>>>,
+        >,
+    ),
 }
 
 impl AllTransducers {
     /// Create a new iterator through all transducers of a particular `size`.
-    pub fn new(size: usize) -> AllTransducers {
+    pub fn new() -> AllTransducers {
         AllTransducers {
-            state: TCycle::new((vec![2; size], vec![vec![size; 2]; size]), true),
+            state: (1, TCycle::new((vec![2; 1], vec![vec![1; 2]; 1]), true)),
         }
     }
 }
@@ -332,15 +413,25 @@ impl Iterator for AllTransducers {
     type Item = Transducer;
 
     fn next(&mut self) -> Option<Transducer> {
-        match self.state.next() {
-            None => None,
+        match self.state.1.next() {
+            None => {
+                self.state.0 += 1;
+                self.state.1 = TCycle::new(
+                    (
+                        vec![2; self.state.0],
+                        vec![vec![self.state.0; 2]; self.state.0],
+                    ),
+                    true,
+                );
+                self.next()
+            }
             Some((flip, transition)) => Some(Transducer {
                 transition: transition
                     .into_iter()
                     .rev()
                     .map(|x| x.into_iter().rev().collect())
                     .collect(),
-                flip: flip.into_iter().rev().collect(),
+                flip: flip.into_iter().map(|x| 1 - x).collect(),
             }),
         }
     }
@@ -363,7 +454,7 @@ fn distinguish(class: &FxHashSet<Transducer>, depth: usize) -> Vec<FxHashSet<Tra
     }
     return res;
 }
-
+/*
 /// Determine whether a transducer's orbit is the maximal cycle.
 fn maximal(m: &Transducer) -> bool {
     let mut trace: Vec<u8> = vec![0; m.transition.len()];
@@ -416,29 +507,69 @@ fn self_branching(m: &Transducer, depth: usize) -> bool {
             && r.canonicalize() == m.canonicalize()
             && finitely_branching(&l, depth - 1));
 }
+*/
 
 /// Divide transducers of a given size into *-equal classes.
-pub fn classify_transducers(size: usize, depth: usize) -> Vec<FxHashSet<Transducer>> {
+pub fn classify_transducers(depth: usize) -> Vec<FxHashSet<Transducer>> {
     let mut pre_classes = Vec::new();
     let mut classes = Vec::new();
     let mut initial = FxHashSet::default();
     let mut key = FxHashMap::default();
-    let mut seen = 0;
-    for (i, m) in AllTransducers::new(size)
-        .map(|x| x.minimize().canonicalize())
+    // let mut seen = 0;
+
+    let mut ctx = PrinterContext::default();
+    ctx.always_inline();
+    for (i, m) in AllTransducers::new()
         .enumerate()
+        .map(|(i, x)| (i, x.minimize().canonicalize()))
     {
-        if i - seen > 100 {
-            seen = i;
-            print!("{}\r", i);
+        if i >= 66 {
+            break;
         }
         if initial.contains(&m) {
             continue;
         } else {
             initial.insert(m.clone());
-            key.insert(m, i);
+            key.insert(m.clone(), i);
+
+            let m2 = m.clone();
+            for _ in 1..9 {
+                if m2.transition.len() > 12 {
+                    break;
+                }
+                /* let g = m2.graph();
+                exec(
+                    g,
+                    &mut ctx,
+                    vec![
+                        CommandArg::Custom("-Gdpi=300".to_string()),
+                        //CommandArg::Custom("-Kcirco".to_string()),
+                        CommandArg::Format(Format::Png),
+                        CommandArg::Output(format!("images/m_{}_{}.png", i, j).to_string()),
+                    ],
+                )
+                .unwrap();
+                m2 = m2.product(&m); */
+            }
+            let mut depth = 6;
+            if i == 27 || i == 23 {
+                depth = 16;
+            }
+            let g = m.orbit_tree(depth);
+            exec(
+                g,
+                &mut ctx,
+                vec![
+                    CommandArg::Custom("-Gdpi=300".to_string()),
+                    CommandArg::Format(Format::Png),
+                    //CommandArg::Custom("-Ktwopi".to_string()),
+                    CommandArg::Output(format!("images/t_{}.png", i).to_string()),
+                ],
+            )
+            .unwrap();
         }
     }
+    /*
     let preds = vec![maximal];
     for (i, pred) in preds.iter().enumerate() {
         let (l, r): (Vec<_>, Vec<_>) = initial.into_par_iter().partition(pred);
@@ -448,6 +579,7 @@ pub fn classify_transducers(size: usize, depth: usize) -> Vec<FxHashSet<Transduc
         }
         initial = r.into_iter().map(|x| x.clone()).collect();
     }
+    */
     classes.push(initial);
     for i in 1..depth + 1 {
         classes = classes
@@ -456,6 +588,7 @@ pub fn classify_transducers(size: usize, depth: usize) -> Vec<FxHashSet<Transduc
             .collect::<Vec<_>>()
             .concat();
     }
+    /*
     let mut ctx = PrinterContext::default();
     ctx.always_inline();
     for class in &classes {
@@ -499,6 +632,7 @@ pub fn classify_transducers(size: usize, depth: usize) -> Vec<FxHashSet<Transduc
             }
         }
     }
+    */
     classes.append(&mut pre_classes);
     return classes;
 }
